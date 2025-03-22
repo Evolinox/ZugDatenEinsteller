edb894a692c8fbf6b9c4e2b2a4be7028c04da2f5
Pascal Jedicke <pascal.jedicke@icloud.com>
2025-03-23 00:20:47 +0100

    feat: updated combobox

--- src/views/Formation.vue
+++ src/views/Formation.vue
@@ -1,15 +1,16 @@
 <script setup lang="ts">
-import { cn } from '@/lib/utils'
+import {cn} from '@/lib/utils'
+import { Button } from '@/components/ui/button'
 import { Card, CardContent } from "@/components/ui/card";
 import { Label } from "@/components/ui/label";
-import { Combobox, ComboboxAnchor, ComboboxEmpty, ComboboxGroup, ComboboxInput, ComboboxItem, ComboboxItemIndicator, ComboboxList } from '@/components/ui/combobox'
+import { Combobox, ComboboxAnchor, ComboboxEmpty, ComboboxGroup, ComboboxInput, ComboboxItem, ComboboxItemIndicator, ComboboxList, ComboboxTrigger } from '@/components/ui/combobox'
 import { NumberField, NumberFieldContent, NumberFieldDecrement, NumberFieldIncrement, NumberFieldInput } from '@/components/ui/number-field'
 
-import { Check, Search } from 'lucide-vue-next'
+import {Check, ChevronsUpDown, Search} from 'lucide-vue-next'
 
 import formations from '../assets/formations.json';
 
-import { ref, watch } from 'vue'
+import {ref, watch} from 'vue'
 
 const props = defineProps<{
     formation: Array<{ locoId: string; brakeIndex: number }>
@@ -22,6 +23,7 @@ const emit = defineEmits<{
 }>()
 
 const formation = ref(props.formation)
+const selectedFormation = ref();
 const vmax = ref(props.vmax)
 
 watch(formation, (newVal) => {
@@ -39,24 +41,33 @@ watch(vmax, (newVal) => {
                 <NumberField id="vmax" v-model="vmax" :default-value="120" :min="10" :max="300" :step="10">
                     <Label for="vmax">Höchstgeschwindigkeit lt. Fpl.:</Label>
                     <NumberFieldContent>
-                        <NumberFieldDecrement />
-                        <NumberFieldInput />
-                        <NumberFieldIncrement />
+                        <NumberFieldDecrement/>
+                        <NumberFieldInput/>
+                        <NumberFieldIncrement/>
                     </NumberFieldContent>
                 </NumberField>
             </div>
             <div class="space-y-1 pt-4">
                 <Label for="formation-select">Suche nach Lokomotiven oder Wagen...</Label>
-                <Combobox id="formation-select" by="label" v-model="formation">
-                    <ComboboxAnchor>
-                        <div class="relative w-[350px] items-center">
-                            <ComboboxInput class="pl-9" :display-value="(val) => val?.short ?? ''" placeholder="Wähle Formation..." />
+                <Combobox id="formation-select" v-model="formation" by="label">
+                    <ComboboxAnchor class="w-[350px]" as-child>
+                        <ComboboxTrigger as-child>
+                            <Button variant="outline" class="flex justify-between items-center gap-2">
+                                <span class="truncate">
+                                    {{ formation?.short ?? 'Wähle Formation' }}
+                                </span>
+                                <ChevronsUpDown class="ml-2 h-4 w-4 shrink-0 opacity-50"/>
+                            </Button>
+                        </ComboboxTrigger>
+                    </ComboboxAnchor>
+                    <ComboboxList class="w-[350px]">
+                        <div class="relative w-full max-w-sm items-center">
+                            <ComboboxInput class="pl-9 focus-visible:ring-0 border-0 border-b rounded-none h-10"
+                                           placeholder="Wähle Formation..."/>
                             <span class="absolute start-0 inset-y-0 flex items-center justify-center px-3">
-                              <Search class="size-4 text-muted-foreground" />
+                                <Search class="size-4 text-muted-foreground"/>
                             </span>
                         </div>
-                    </ComboboxAnchor>
-                    <ComboboxList class="ml-32 w-[350px]">
                         <ComboboxEmpty>
                             Keine Formation gefunden.
                         </ComboboxEmpty>
@@ -64,7 +75,7 @@ watch(vmax, (newVal) => {
                             <ComboboxItem v-for="f in formations.formationen" :key="f.id" :value="f">
                                 {{ f.short }}
                                 <ComboboxItemIndicator>
-                                    <Check :class="cn('ml-auto h-4 w-4')" />
+                                    <Check :class="cn('ml-auto h-4 w-4')"/>
                                 </ComboboxItemIndicator>
                             </ComboboxItem>
                         </ComboboxGroup>
